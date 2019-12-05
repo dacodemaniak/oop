6adcccede689bd087e47bf59030c2f448e0367ff
Jean-Luc <jean-luc.a@ideafactory.fr>
2019-12-05 16:00:30 +0100

    Added toggler on create receipe button

--- src/main.ts
+++ src/main.ts
@@ -1,79 +1,21 @@
-import { StrategyInterface } from "./strategies/products/strategy-interface";
-import { NameStragegy } from "./strategies/products/name-strategy";
-import { NamePriceStragegy } from "./strategies/products/name-price-strategy";
-import { NamePriceUnitStrategy } from "./strategies/products/name-price-unit";
-import { Recette } from "./models/recette";
-import { QuantityProduct } from "./models/quantity-product";
+import * as $ from 'jquery';
+import { ReceipeFormModule } from './modules/receipe-form-module';
+
 /**
  * @name Main
  * @author Aélion - Déc. 2019 - jla.webprojet@gmail.com
- * @package /
- * @version 1.0.0
- * Entry point of the application
+ * @package
+ * @version 1.1.0
  */
+export class Main {
+    public constructor() {
+        new ReceipeFormModule();
+    }
+}
 
- import "./scss/main.scss";
- 
- const strategy: StrategyInterface = new NameStragegy();
-
- let produit1: QuantityProduct = new QuantityProduct();
- produit1.setStrategy(strategy);
-produit1.setName('Farine');
-produit1.setBaseUnit('kg');
-produit1.setPrice(1.80)
-produit1.setQuantity(300);
-produit1.setUnit('g');
-
-let produit2: QuantityProduct = new QuantityProduct();
-produit2.setStrategy(strategy);
-produit2.setName('Lait');
-produit2.setBaseUnit('l');
-produit2.setPrice(0.94);
-produit2.setQuantity(60);
-produit2.setUnit('cl');
-
-let produit3: QuantityProduct = new QuantityProduct();
-produit3.setName('Oeufs');
-produit3.setBaseUnit('unité');
-produit3.setPrice(0.22);
-produit3.setQuantity(3);
-produit3.setUnit('unité');
-
-let produit4: QuantityProduct = new QuantityProduct();
-produit4.setName('Sucre');
-produit4.setBaseUnit('kg');
-produit4.setPrice(0.69);
-produit4.setQuantity(5);
-produit4.setUnit('g');
-
-let produit5: QuantityProduct = new QuantityProduct();
-produit5.setName('Rhum');
-produit5.setBaseUnit('cl');
-produit5.setPrice(9.65);
-produit5.setQuantity(5);
-produit5.setUnit('cl');
-produit5.setQuantityUnit(70);
-
-let produit6: QuantityProduct = new QuantityProduct();
-produit6.setName('Beurre');
-produit6.setBaseUnit('kg');
-produit6.setPrice(19.60);
-produit6.setQuantity(50);
-produit6.setUnit('g');
-
-const lesCrepes: Recette = new Recette('Crèpes');
-lesCrepes.setQuantity(15);
-
-lesCrepes.addProduct(produit1);
-lesCrepes.addProduct(produit2);
-lesCrepes.addProduct(produit3);
-lesCrepes.addProduct(produit4);
-lesCrepes.addProduct(produit5);
-lesCrepes.addProduct(produit6);
-
-const lesGaufres: Recette = new Recette('Gauffres');
-lesGaufres.addProduct(produit1);
-
-console.log(lesCrepes.toString());
-
-console.log(lesGaufres.toString());
+// App bootstraping with jQuery
+// Create a new instance of the Main class
+// after the document was completely loaded
+$(document).ready(() => {
+   const app: Main = new Main(); 
+});
---
+++ src/modules/receipe-form-module.ts
@@ -0,0 +1,24 @@
+import * as $ from 'jquery';
+
+export class ReceipeFormModule {
+    public constructor() {
+        this.setCreateButtonHandler();
+    }
+
+    private setCreateButtonHandler(): void {
+        $('#create-receipe').on(
+            'click',
+            (event: any): void => this.toggleIngredientForm(event)
+        );
+    }
+
+    private toggleIngredientForm(event: any): void {
+        if ($('#ingredient-form').hasClass('hidden-form')) {
+            // Have to remove the hidden-form class
+            $('#ingredient-form').removeClass('hidden-form');
+        } else {
+            // Have to add the hidden-form class
+            $('#ingredient-form').addClass('hidden-form');
+        }
+    }
+}
